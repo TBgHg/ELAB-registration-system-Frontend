34f3b5c5b28ed0be12cc9dd61658ca02db906fd5
zeithrold <zeithrold@gmail.com>
2023-05-12 09:49:23 +0800

    removed unnecessary params

--- pages/main/tab/space/popup/SearchPage.tsx
+++ pages/main/tab/space/popup/SearchPage.tsx
@@ -2,7 +2,6 @@ import NavigationCloseAction from "@/components/NavigationCloseAction";
 import SpaceListItem from "@/components/space/home/SpaceListItem";
 import useSpaceSearch from "@/libs/hooks/useSpaceSearch";
 import { store } from "@/libs/store";
-import type { PopupNavigatorScreenProps } from "@/navigators/main/tab/space/popup";
 import { useFocusEffect } from "@react-navigation/native";
 import {
   Icon,
@@ -42,65 +41,63 @@ const styles = StyleSheet.create({
   },
 });
 
-const SearchPage = observer(
-  ({ navigation }: PopupNavigatorScreenProps<"SpaceSearchPage">) => {
-    const [query, setQuery] = React.useState("");
-    const accessToken = store.user.credential.accessToken;
-    const { data, isLoading, isValidating, mutate } = useSpaceSearch(
-      accessToken,
-      query
-    );
-    useFocusEffect(
-      React.useCallback(() => {
-        mutate().catch((err) => {
-          console.error(err);
-        });
-      }, [])
-    );
-    return (
-      <Layout style={styles.root}>
-        <TopNavigation
-          title="搜索空间"
-          alignment="center"
-          accessoryLeft={NavigationCloseAction}
+const SearchPage = observer(() => {
+  const [query, setQuery] = React.useState("");
+  const accessToken = store.user.credential.accessToken;
+  const { data, isLoading, isValidating, mutate } = useSpaceSearch(
+    accessToken,
+    query
+  );
+  useFocusEffect(
+    React.useCallback(() => {
+      mutate().catch((err) => {
+        console.error(err);
+      });
+    }, [])
+  );
+  return (
+    <Layout style={styles.root}>
+      <TopNavigation
+        title="搜索空间"
+        alignment="center"
+        accessoryLeft={NavigationCloseAction}
+      />
+      <ScrollView style={styles.container}>
+        <Input
+          value={query}
+          onChangeText={setQuery}
+          size="large"
+          textStyle={{
+            fontSize: 18,
+          }}
+          accessoryLeft={
+            <View>
+              <Icon name="search-outline" fill="#8F9BB3" />
+            </View>
+          }
         />
-        <ScrollView style={styles.container}>
-          <Input
-            value={query}
-            onChangeText={setQuery}
-            size="large"
-            textStyle={{
-              fontSize: 18,
-            }}
-            accessoryLeft={
-              <View>
-                <Icon name="search-outline" fill="#8F9BB3" />
-              </View>
-            }
-          />
-          <Layout style={styles.spinner}>
-            {isLoading || isValidating ? <Spinner /> : null}
-          </Layout>
-          <Layout>
-            {data === undefined ? (
-              <Layout style={styles.empty}>
-                <Text appearance="hint">在搜索栏输入以开始...</Text>
-              </Layout>
-            ) : (
-              data.map((space) => {
-                return (
-                  <SpaceListItem
-                    key={`spaceSearchResult-${space.space_id}`}
-                    space={space}
-                  />
-                );
-              })
-            )}
-          </Layout>
-        </ScrollView>
-      </Layout>
-    );
-  }
-);
+        <Layout style={styles.spinner}>
+          {isLoading || isValidating ? <Spinner /> : null}
+        </Layout>
+        <Layout>
+          {data === undefined ? (
+            <Layout style={styles.empty}>
+              <Text appearance="hint">在搜索栏输入以开始...</Text>
+            </Layout>
+          ) : (
+            data.map((space) => {
+              return (
+                <SpaceListItem
+                  key={`spaceSearchResult-${space.space_id}`}
+                  space={space}
+                />
+              );
+            })
+          )}
+        </Layout>
+      </ScrollView>
+    </Layout>
+  );
+});
 
 export default SearchPage;
